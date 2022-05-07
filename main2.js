03aafff852a19fd263d081d7b930ac57cea94c54
my171593 <85271052+my171593@users.noreply.github.com>
2022-05-06 22:01:00 -0400

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,46 @@
+Img="";
+status="";
+object=[];
+function preload(){
+Img=loadImage("bathroom.jpg");
+}
+
+function setup(){
+Canvas=createCanvas(650,600);
+Canvas.position(550,150);
+objectdetector=ml5.objectDetector("cocossd",modelloaded);
+ document.getElementById("status").innerHTML="status-detecting objects";
+}
+
+function draw(){
+image(Img,0,0,650,600);
+if(status!=""){
+for(i=0;i<object.length;i++){
+document.getElementById("status").innerHTML="status-object detected";
+fill("red");
+percent=floor(object[i].confidence*100);
+text(object[i].label+" " +percent+"%",object[i].x,object[i].y);
+noFill();
+stroke("red");
+rect(object[i].x,object[i].y,object[i].width,object[i].height);
+}    
+}
+
+}
+
+function modelloaded(){
+console.log("model is loaded");
+status=true;
+objectdetector.detect(Img,gotresult);
+}
+
+function gotresult(error,results){
+if(error){
+console.log(error);
+}  
+console.log(results);
+object=results;
+
+}
+
+
---
+++ mainindex.js
@@ -0,0 +1,11 @@
+function Bedroom(){
+window.location="pick-one.html"
+}
+
+function LivingRoom(){
+    window.location="pick-2.html"
+    }
+
+ function WashRoom(){
+ window.location="pick-3.html"
+}
